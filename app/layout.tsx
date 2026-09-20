e0d4c7360df5ffa23c8735a7144058bb25f445cc
keithooVxbes <zrifqyab@gmail.com>
2026-09-20 15:26:48 +0700

    Update title and add thumbnail for vulnerability scanning project

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from '@/components/ui/toaster'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Zayyan | Personal Portfolio',
+  title: 'myoumiaw | Personal Portfolio',
   description: 'Portfolio profesional dalam bidang cybersecurity, penetration testing, dan web development',
   generator: 'zayyan.dev',
 }
